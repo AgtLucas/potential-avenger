2ec8d612c633763c4be3aea061e2230ffff45150
Lucas Churchill <lucas@agtlucas.com>
2014-07-26 16:46:50 -0300

    Update routes

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products do
+    get "delete"
+  end
+
+  root to: "products#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
